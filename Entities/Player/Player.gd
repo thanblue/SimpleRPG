5e429fa7e110fe2314cf69510100fd9a551eae8d
Nguyen Van Thanh <thanhnguyenvan@vnpt.vn>
2020-03-12 16:23:51 +0700

    Phat trien tiep muc so 5

--- Entities/Player/Player.gd
+++ Entities/Player/Player.gd
@@ -2,8 +2,29 @@ extends KinematicBody2D
 
 # Player movement speed
 export var speed = 75
+var last_direction = Vector2(0,1)
+var attack_playing = false
+var health = 100
+var health_max = 100
+var health_regeneration = 1
+var mana = 100
+var mana_max = 100
+var mana_regeneration = 2
 
+signal player_stats_changed
+func _ready():
+	emit_signal("player_stats_changed", self)
 func _physics_process(delta):
+	
+	var new_mana = min(mana + mana_regeneration * delta, mana_max)
+	if new_mana != mana:
+		mana = new_mana
+		emit_signal("player_stats_changed", self)
+		
+	var new_health = min(health + health_regeneration * delta, health_max)
+	if new_health != health:
+		health = new_health
+		emit_signal("play_stats_changed",self)
 	# Get player input
 	var direction: Vector2
 	direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -14,5 +35,49 @@ func _physics_process(delta):
 		direction = direction.normalized()
 	
 	# Apply movement
+	
 	var movement = speed * direction * delta
+	if attack_playing:
+		movement = 0.3 * movement
+	
 	move_and_collide(movement)
+	if not attack_playing:
+		animates_player(direction)
+	
+	
+func animates_player(direction : Vector2):
+	if direction != Vector2.ZERO:
+		last_direction = direction
+		var animation = get_animation_direction(last_direction) + "_walk"
+		$Sprite.play(animation)
+	else:
+		var animation = get_animation_direction(last_direction) + "_idle"
+		$Sprite.play(animation)
+		
+func get_animation_direction(direction : Vector2):
+	var norm_direction = direction.normalized()
+	if norm_direction.y > 0.707:
+		return "down"
+	elif norm_direction.y <= -0.707:
+		return "up"
+	elif norm_direction.x <=0.707:
+		return "left"
+	elif norm_direction.x >= 0.707:
+		return "right"
+	return "down"
+func _input(event):
+	if event.is_action_pressed("attack"):
+		attack_playing = true
+		var animation = get_animation_direction(last_direction) + "_attack"
+		$Sprite.play(animation)
+	elif event.is_action_pressed("fireball"):
+		if mana >= 25:
+			mana = mana - 25
+			emit_signal("player_stats_changed", self)
+		attack_playing = true
+		var animation = get_animation_direction(last_direction) +  "_fireball"	
+		$Sprite.play(animation)
+
+
+func _on_Sprite_animation_finished():
+	attack_playing = false
